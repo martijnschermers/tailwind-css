9fae2051b8783ea378b23f13ab0b77e7e7fe341a
Martijn <martijnschermers2@gmail.com>
2022-05-25 11:58:38 +0200

    Finished Tailwind tutorial Fireship

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,19 +1,25 @@
+import { DiGithubBadge, DiHtml5, DiLinux, DiVisualstudio, DiJsBadge } from 'react-icons/di';
+
 const SideBar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-primary text-white shadow-lg">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+        <div className="dark fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-100 text-gray-900 dark:bg-primary dark:text-white shadow-lg">
+            <SideBarIcon icon={<DiGithubBadge size="30"/>}/>
+            <SideBarIcon icon={<DiHtml5 size="30"/>}/>
+            <SideBarIcon icon={<DiLinux size="30"/>}/>
+            <SideBarIcon icon={<DiVisualstudio size="30"/>}/>
+            <SideBarIcon icon={<DiJsBadge size="30"/>}/>
         </div>
     ); 
 }; 
 
-const SideBarIcon = ({ icon }) => (
-    <div className="sidebar-icon">
+const SideBarIcon = ({ icon, text = 'tooltip' }) => (
+    <div className="sidebar-icon group">
         {icon}
+
+        <span className="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
     </div>
-)
+);
 
 export default SideBar; 
